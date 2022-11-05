7c512aaae6e0766f043a0e252ce7e0a6f541a693
xchavez94x <66273760+xchavez94x@users.noreply.github.com>
2022-11-05 01:11:53 -0700

    init: search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Input';
+import styles from "./styles.module.css";
 
 export default function Seacrh() {
     return (
-        <div>Searcg</div>
+        <div className={styles.container}>
+            <h1>
+                Search jobs 
+            </h1>
+            <Input />
+        </div>
     )
 }
